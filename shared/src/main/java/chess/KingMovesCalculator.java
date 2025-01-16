01596f2af155e4042f90dfc127c9076f4c4d8cf2
Heath Woodruff <happyheathbar@gmail.com>
2025-01-15 17:18:01 -0700

    Implimented ChessBoard currently debugging

--- shared/src/main/java/chess/KingMovesCalculator.java
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -1,4 +1,14 @@
 package chess;
 
+import java.util.Collection;
+import java.util.Iterator;
+
 public class KingMovesCalculator {
+
+    public KingMovesCalculator() {
+    }
+
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        throw new RuntimeException("Not implemented");
+    }
 }
