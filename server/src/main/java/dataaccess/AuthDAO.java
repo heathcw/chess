ee12e3f80fa41effb13d0a8dfb1ad0c674a1eace
Heath Woodruff <happyheathbar@gmail.com>
2025-02-19 14:25:09 -0700

    implimenting DAO interfaces

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,4 +1,9 @@
 package dataaccess;
 
+import model.AuthData;
+
 public interface AuthDAO {
+    void createAuth(AuthData data);
+    AuthData getAuth(String authToken);
+    void deleteAuth(AuthData data);
 }
--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,4 +1,12 @@
 package dataaccess;
 
+import model.GameData;
+
+import java.util.ArrayList;
+
 public interface GameDAO {
+    void createGame(String gameName);
+    GameData getGame();
+    ArrayList<GameData> listGames();
+    void updateGame(GameData data);
 }
