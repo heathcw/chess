0e9d1650f2a2760c2612098f76a3f89e3c99468a
Heath Woodruff <happyheathbar@gmail.com>
2025-02-20 14:42:50 -0700

    implemented MemoryUserDAO

---
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -0,0 +1,23 @@
+package dataaccess;
+
+import model.UserData;
+
+import java.util.ArrayList;
+
+public class MemoryUserDAO implements UserDAO {
+
+    private ArrayList<UserData> users = new ArrayList<>();
+
+    public void createUser(UserData data) {
+        users.add(data);
+    }
+
+    public UserData getUser(String username) {
+        for (UserData user: users) {
+            if (user.username().equals(username)) {
+                return user;
+            }
+        }
+        return null;
+    }
+}
