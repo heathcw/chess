4ce264424f369b744f6e0f0f856836689962338b
Heath Woodruff <happyheathbar@gmail.com>
2025-03-17 14:54:02 -0600

    created first facade test

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,25 @@
 package client;
 
+import exception.ResponseException;
 import org.junit.jupiter.api.*;
 import server.Server;
+import service.RegisterRequest;
+import service.RegisterResult;
+import ui.ServerFacade;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
-        var port = server.run(0);
+        var port = server.run(1080);
         System.out.println("Started test HTTP server on " + port);
+        var serverUrl = "http://localhost:1080";
+        facade = new ServerFacade(serverUrl);
     }
 
     @AfterAll
@@ -22,8 +29,10 @@ public class ServerFacadeTests {
 
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void registerTest() throws ResponseException {
+        RegisterRequest request = new RegisterRequest("user", "pass", "em");
+        RegisterResult result = facade.register(request);
+        assert result.username().equals("user");
     }
 
 }
