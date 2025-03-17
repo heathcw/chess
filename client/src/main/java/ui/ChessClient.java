20b20641ce7af45e0ec9868a0c7e4d2b786c846c
Heath Woodruff <happyheathbar@gmail.com>
2025-03-17 14:44:20 -0600

    implementing create and list functions

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -1,14 +1,16 @@
 package ui;
 
+import java.util.Arrays;
+
+import com.google.gson.Gson;
 import exception.ResponseException;
-import service.LoginRequest;
-import service.LoginResult;
-import service.RegisterRequest;
-import service.RegisterResult;
+import model.GameData;
+import service.*;
 
 public class ChessClient {
 
     private String user = null;
+    private String authToken = null;
     private final ServerFacade server;
     private final String serverUrl;
     private State state = State.SIGNEDOUT;
@@ -18,15 +20,34 @@ public class ChessClient {
         this.serverUrl = url;
     }
 
+    public String eval(String input) {
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "register" -> register(params);
+                case "login" -> login(params);
+                case "quit" -> "quit";
+                case "create" -> create(params);
+                case "list" -> list();
+                default -> help();
+            };
+        } catch (ResponseException ex) {
+            return ex.getMessage();
+        }
+    }
+
     public String register(String... params) throws ResponseException {
         if (params.length == 3) {
             state = State.SIGNEDIN;
             RegisterRequest request = new RegisterRequest(params[0], params[1], params[2]);
             RegisterResult result = server.register(request);
             user = result.username();
+            authToken = result.authToken();
             return String.format("You signed in as %s.", user);
         }
-        throw new ResponseException(400, "Expected: <username> <password> <email>");
+        throw new ResponseException(400, "Expected: <USERNAME> <PASSWORD> <EMAIL>");
     }
 
     public String login(String... params) throws ResponseException {
@@ -35,9 +56,32 @@ public class ChessClient {
             LoginRequest request = new LoginRequest(params[0], params[1]);
             LoginResult result = server.login(request);
             user = result.username();
+            authToken = result.authToken();
             return String.format("You signed in as %s.", user);
         }
-        throw new ResponseException(400, "Expected: <username> <password>");
+        throw new ResponseException(400, "Expected: <USERNAME> <PASSWORD>");
+    }
+
+    public String create(String... params) throws ResponseException {
+        assertSignedIn();
+        if (params.length == 1) {
+            GameRequest request = new GameRequest(params[0], authToken);
+            server.createGame(request);
+            return String.format("You created game: %s.", request.gameName());
+        }
+        throw new ResponseException(400, "Expected: <NAME>");
+    }
+
+    public String list() throws ResponseException {
+        assertSignedIn();
+        AuthRequest request = new AuthRequest(authToken);
+        ListResult result = server.listGames(request);
+        var list = new StringBuilder();
+        var gson = new Gson();
+        for (GameData game : result.games()) {
+            list.append(gson.toJson(game)).append('\n');
+        }
+        return list.toString();
     }
 
     public String help() {
