3e3e94a8cb0646aa4fa232010ca4f2fc1e80c2e5
Heath Woodruff <happyheathbar@gmail.com>
2025-03-24 13:27:03 -0600

    updating port

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -15,9 +15,9 @@ public class ServerFacadeTests {
     @BeforeAll
     public static void init() {
         server = new Server();
-        var port = server.run(8080);
+        var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
-        var serverUrl = "http://localhost:8080";
+        var serverUrl = "http://localhost:" + port;
         facade = new ServerFacade(serverUrl);
         try {
             facade.clear();
