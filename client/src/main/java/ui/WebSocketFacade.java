deefd17e433862e4e6234a671042f2722179ebfe
Heath Woodruff <happyheathbar@gmail.com>
2025-04-03 18:26:58 -0600

    websocketfacade and notificationhandler

--- client/src/main/java/ui/WebSocketFacade.java
+++ client/src/main/java/ui/WebSocketFacade.java
@@ -2,6 +2,8 @@ package ui;
 
 import exception.ResponseException;
 import com.google.gson.Gson;
+import websocket.messages.NotificationMessage;
+
 import javax.websocket.*;
 import java.io.IOException;
 import java.net.URI;
@@ -10,22 +12,22 @@ import java.net.URISyntaxException;
 public class WebSocketFacade extends Endpoint {
 
     Session session;
+    NotificationHandler notificationHandler;
 
-    public WebSocketFacade(String url) throws ResponseException {
+    public WebSocketFacade(String url, NotificationHandler notificationHandler) throws ResponseException {
         try {
             url = url.replace("http", "ws");
             URI socketURI = new URI(url + "/ws");
-            //this.notificationHandler = notificationHandler;
+            this.notificationHandler = notificationHandler;
 
             WebSocketContainer container = ContainerProvider.getWebSocketContainer();
             this.session = container.connectToServer(this, socketURI);
 
-            //set message handler
             this.session.addMessageHandler(new MessageHandler.Whole<String>() {
                 @Override
                 public void onMessage(String message) {
-                    //Notification notification = new Gson().fromJson(message, Notification.class);
-                    //notificationHandler.notify(notification);
+                    NotificationMessage notification = new Gson().fromJson(message, NotificationMessage.class);
+                    notificationHandler.notify(notification);
                 }
             });
         } catch (DeploymentException | IOException | URISyntaxException ex) {
