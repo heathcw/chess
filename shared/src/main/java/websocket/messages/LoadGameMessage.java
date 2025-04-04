d077c11d851f7ff0c7c35e24d8f94d9249057380
Heath Woodruff <happyheathbar@gmail.com>
2025-04-03 18:21:47 -0600

    extended server messages

---
+++ shared/src/main/java/websocket/messages/LoadGameMessage.java
@@ -0,0 +1,13 @@
+package websocket.messages;
+
+public class LoadGameMessage extends ServerMessage {
+
+    private final String game;
+
+    public LoadGameMessage(String game) {
+        super(ServerMessageType.LOAD_GAME);
+        this.game = game;
+    }
+
+    public String getGame() {return this.game;}
+}
